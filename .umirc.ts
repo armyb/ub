384e008c5bdf2900634a4d9fb54bac4fe3317d86
zhouhuajian <zhouhuajian@chebada.com>
2021-02-04 17:47:57 +0800

    umirc-config

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'umi';
 
+const { NODE_ENV } = process.env;
+console.log("🚀 ~ file: .umirc.ts ~ line 4 ~ NODE_ENV", NODE_ENV)
+
 export default defineConfig({
   routes: [
     { path: '/', component: '@/pages/index' },
   ],
   // outputPath: 'build',
-  publicPath: 'https://armyb.github.io/ub/',
+  publicPath: NODE_ENV === 'development' ? './' : 'https://armyb.github.io/ub/',
   history:{ type:'hash' }
 });
